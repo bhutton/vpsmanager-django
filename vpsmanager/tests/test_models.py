93551f2267a027ae338addfc1a971f6c2d665662
Benjamin Hutton <ben@benhutton.com.au>
2017-10-09 19:45:42 +0800

    Added test for iterable for vps_list model

--- vpsmanager/tests/test_models.py
+++ vpsmanager/tests/test_models.py
@@ -1,13 +1,16 @@
-from django.template.loader import render_to_string
-from django.core.urlresolvers import resolve
+#from django.template.loader import render_to_string
+#from django.core.urlresolvers import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from django.utils.html import escape
+#from django.http import HttpRequest
+#from django.utils.html import escape
+
+import collections
 
 from vps.models import VPS
 
 class VPSInventoryTest(TestCase):
     def test_list_vps(self):
         vps = VPS()
-        assert vps.get_vps_list()
+        list = vps.get_vps_list()
+        assert isinstance(list, collections.Iterable)
 
