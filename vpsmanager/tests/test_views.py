94b869a224a7664f3f262b6139e64c2adc0ae493
Benjamin Hutton <ben@benhutton.com.au>
2017-10-09 09:08:02 +0800

    Create view for createvps

---
+++ vpsmanager/tests/test_views.py
@@ -0,0 +1,28 @@
+from django.template.loader import render_to_string
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from django.http import HttpRequest
+from django.utils.html import escape
+
+from vps.views import home_page
+
+
+from unittest import skip
+
+class HomePageTest(TestCase):
+
+    def test_home_page_renders_homepage(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+class CreateVPSTest(TestCase):
+
+    def test_create_vps_renders_form(self):
+        response = self.client.get('/createvps/')
+        self.assertTemplateUsed(response, 'createvps.html')
